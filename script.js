988d139a72e42e8d8124b74bb40b0a8a38af44a2
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-15 13:58:05 +0200

    Added function calculate total money

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function getRandomUser() {
 // double everyone money
 function doubleMoney() {
     data = data.map(user => {
-        return {...user, money: user.money * 2};
+        return { ...user, money: user.money * 2 };
     });
 
     updateDOM();
@@ -43,6 +43,15 @@ function showMillionaires() {
     data = data.filter(user => user.money > 1000000);
 
     updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
 }
 
 // add new obj to data arr
@@ -73,5 +82,6 @@ function formatMoney(number) {
 // event listeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
-sortBtn.addEventListener('click', sortByRichest);sortBtn.addEventListener('click', sortByRichest);
+sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
