bc03892b9cdc72e9c142be1994e508f3c33f7b64
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-15 11:47:43 +0200

    Added and tested new function. It connect to API and download data of ranodm user next unpack and return user specify to my app

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// fetch random user and add random money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+
+    console.log(newUser);
+}
