7c8f2dbe2a4f2d13fa6ca930647393072f8cad18
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-15 11:25:27 +0200

    Added ref to DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
