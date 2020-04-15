b67084bd7847a5e0226c3b4b76401e5f5ef21351
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-15 12:49:30 +0200

    Added function that format money value

--- script.js
+++ script.js
@@ -41,7 +41,12 @@ function updateDOM(providedData = data) {
     providedData.forEach(item => {
         const element = document.createElement('div');
         element.classList.add('person');
-        element.innerHTML = `<strong>${item.name}</strong>${item.money}`;
+        element.innerHTML = `<strong>${item.name}</strong>${formatMoney(item.money)}`;
         main.appendChild(element);
     });
+}
+
+// format number as money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
